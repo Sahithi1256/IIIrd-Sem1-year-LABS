c58d03883cc5efe8cfa17e8e4c7b33ece43d36d7
Sahithi1256 <94466932+Sahithi1256@users.noreply.github.com>
2023-01-05 21:10:56 +0530

    Create frame_sorting_technique.c

---
+++ DCCN/frame_sorting_technique.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+struct frame{
+
+int num;
+
+char str[20];
+
+};
+
+struct frame arr[10];
+
+int n;
+
+void sort() /*Bubble sort */
+
+{
+
+int i,j;
+
+struct frame temp;
+
+for(i=0;i<n-1;i++)
+
+for(j=0;j<n-i-1;j++)
+
+if(arr[j].num>arr[j+1].num)
+
+{ temp=arr[j];
+
+arr[j]=arr[j+1];
+
+arr[j+1]=temp;
+
+}
+
+}
+
+int main()
+
+{
+
+int i;
+
+printf("Enter the number of frames\n");
+
+scanf("%d",&n);
+
+printf("Enter the frame sequence number and frame contents\n");
+
+for(i=0;i<n;i++)
+
+scanf("%d%d",&arr[i].num,&arr[i].str);
+
+sort();
+
+printf("The frame in sequences\n");
+
+for(i=0;i<n;i++)
+
+printf("%d\t%s\n",arr[i].num,arr[i].str);
+
+}
